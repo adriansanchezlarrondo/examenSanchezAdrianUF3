3acf96c2f6012555dc89e58fc7b5501a27ce4390
adriansanchezlarrondo <sanchezlarrondoadrian@fpllefia.com>
2024-03-08 17:14:59 +0100

    .

--- src/componentes/form.js
+++ src/componentes/form.js
@@ -8,20 +8,25 @@ export const form =  {
         
         <div class="col-6">
             <h3>Grupo</h3>
-            <label for="nombreGrupo" class="label-control">Nombre del grupo:</label>
-            <input id="nombreGrupo" type="text" class="form-control mt-2" placeholder="Borrachos de DAW2" minlength="4" maxlength="10">
-            <label for="numeroMesa" class="label-control">Mesa numero</label>
-            <input id="numeroMesa" type="number" class="form-control mt-2" placeholder ="1" minlength="1" maxlength="15">
-            
-            <h3 class="mt-5">Haz tu pedido</h3>
-            <div class="d-flex gap-3 ">
-                <select name="cervezas" id="cervezas" class="form-control">
-                    <option value="">Selecciona qué birra quieres</option>
-                </select>
-            
-                <input id="cantidad" type="number" value="0" class="form-control">
-            </div>
-            <button id="btnAñadir" class="btn btn-success mt-4 w-100">¡Enviar pedido!</button>
+            <form novalidate action="" method="get" class="">
+                <label for="nombreGrupo" class="label-control">Nombre del grupo:</label>
+                <input id="nombreGrupo" type="text" class="form-control mt-2" placeholder="Borrachos de DAW2" minlength="4" maxlength="10">
+                <div class="invalid-feedback small">El nombre del grupo ha de ser entre 4 y 10 caracteres</div>
+
+                <label for="numeroMesa" class="label-control">Mesa numero</label>
+                <input id="numeroMesa" type="number" class="form-control mt-2" placeholder ="1" minlength="1" maxlength="15">
+                <div class="invalid-feedback small">El numero de la mesa tiene que ser entre 1 y 15</div>
+
+                <h3 class="mt-5">Haz tu pedido</h3>
+                <div class="d-flex gap-3 ">
+                    <select name="cervezas" id="cervezas" class="form-control">
+                        <option value="">Selecciona qué birra quieres</option>
+                    </select>
+                
+                    <input id="cantidad" type="number" value="0" class="form-control">
+                </div>
+                <button class="btn btn-success mt-4 w-100">¡Enviar pedido!</button>
+            </form>
         </div>
         <div class="col-6 border ">
             <div class="p-3 d-flex">
