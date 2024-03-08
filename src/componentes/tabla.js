230e7f0c5b87b9a8aa5dc24de107589391c0fd39
adriansanchezlarrondo <sanchezlarrondoadrian@fpllefia.com>
2024-03-08 15:54:32 +0100

    ejercicio 4

--- src/componentes/tabla.js
+++ src/componentes/tabla.js
@@ -1,11 +1,58 @@
 // import { comandas } from "../main"
 
-import { actualizarTotalPrecio, comandas } from "./form"
+// import { actualizarTotalPrecio, comandas } from "./form"
 
 export const tabla =  {
     template:  //html
     `
-    <div class="card pt-5 m-5 shadow border-0 ">
+    <div class="row">
+        <h1 class="text-center mb-5 ">----- Vista camareros -----</h1>
+        <h3>Pedidos</h3>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Grupo</th>
+                    <th>Mesa</th>
+                    <th>Cerveza</th>
+                    <th>Cantidad</th>
+                    <th>Estado</th>
+                </tr>        
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Borrachos de DAW2</td>
+                    <td>1</td>
+                    <td>Estrella Galicia</td>
+                    <td>3</td>
+                    <td>
+                        <div class="d-flex gap-2">
+                            <button class="btn btn-outline-warning w-100 btn-sm">Pedido pendiente...</button>
+                            <button class="btn btn-outline-danger w-100 btn-sm"> 🗑 Borrar pedido</button>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Cabezones contentos</td>
+                    <td>1</td>
+                    <td>Estrella DAM</td>
+                    <td>2</td>
+                    <td>
+                        <div class="d-flex gap-2">
+                            <button class="btn btn-outline-success w-100 btn-sm">¡Pedido servido!</button>
+                            <button class="btn btn-outline-danger w-100 btn-sm"> 🗑 Borrar pedido</button>
+                        </div>       
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+
+
+    <!--<div class="card pt-5 m-5 shadow border-0 ">
         <div class="p-5">
             <h3 class="">Selecciona tu cerveza y haz tu pedido</h3>
             <table id="tablaPedidos" class="container table table-bordered">
@@ -30,60 +77,60 @@ export const tabla =  {
             </table>
 
         </div>
-    </div>
+    </div>-->
     `,
     script : ()=>{
-        document.querySelector('#tablaPedidos').addEventListener('click', (e) => {
+        // document.querySelector('#tablaPedidos').addEventListener('click', (e) => {
 
-            console.log('Hola desde TablaPedidos')
+        //     console.log('Hola desde TablaPedidos')
             
-            if(e.target.id == 'btnEdit'){
-              console.log('editar', e.target.id)
-              Editar(e)
-            }
+        //     if(e.target.id == 'btnEdit'){
+        //       console.log('editar', e.target.id)
+        //       Editar(e)
+        //     }
 
-            if(e.target.id == 'btnEliminar'){
-              console.log('borrar', e.target.id)
-              Eliminar(e)
-            }
+        //     if(e.target.id == 'btnEliminar'){
+        //       console.log('borrar', e.target.id)
+        //       Eliminar(e)
+        //     }
     
-        })
+        // })
 
-        function Eliminar(e) {
+        // function Eliminar(e) {
 
-            const fila = e.target.closest('tr')
-            const idPedido = parseInt(fila.dataset.pedido)
+        //     const fila = e.target.closest('tr')
+        //     const idPedido = parseInt(fila.dataset.pedido)
 
-            // Crear un nuevo array sin la comanda que se desea eliminar
-            const comandasFiltradas = comandas.filter(pedido => pedido.id !== idPedido);
-            console.log('comandasFiltradas', comandasFiltradas);
-            // Actualizar el array comandas con el nuevo array filtrado
-            comandas.length = 0;
-            comandas.push(...comandasFiltradas);
-            console.log('comandas', comandas)
-            // Eliminar la fila de la tabla
-            fila.remove();
+        //     // Crear un nuevo array sin la comanda que se desea eliminar
+        //     const comandasFiltradas = comandas.filter(pedido => pedido.id !== idPedido);
+        //     console.log('comandasFiltradas', comandasFiltradas);
+        //     // Actualizar el array comandas con el nuevo array filtrado
+        //     comandas.length = 0;
+        //     comandas.push(...comandasFiltradas);
+        //     console.log('comandas', comandas)
+        //     // Eliminar la fila de la tabla
+        //     fila.remove();
             
-            actualizarTotalPrecio(comandasFiltradas.precioTotal)
-        }
+        //     actualizarTotalPrecio(comandasFiltradas.precioTotal)
+        // }
 
-        function Editar(e) {
+        // function Editar(e) {
 
-            const fila = e.target.closest('tr')
-            const comandaId = parseInt(fila.dataset.comandaid)
+        //     const fila = e.target.closest('tr')
+        //     const comandaId = parseInt(fila.dataset.comandaid)
 
-            // Crear un nuevo array sin la comanda que se desea eliminar
-            const comandasFiltradas = comandas.filter(comanda => comanda.id !== comandaId);
+        //     // Crear un nuevo array sin la comanda que se desea eliminar
+        //     const comandasFiltradas = comandas.filter(comanda => comanda.id !== comandaId);
             
-            // Actualizar el array comandas con el nuevo array filtrado
-            comandas.length = 0;
-            comandas.push(...comandasFiltradas);
-            console.log(comandas)
-            // Eliminar la fila de la tabla
-            fila.remove();
+        //     // Actualizar el array comandas con el nuevo array filtrado
+        //     comandas.length = 0;
+        //     comandas.push(...comandasFiltradas);
+        //     console.log(comandas)
+        //     // Eliminar la fila de la tabla
+        //     fila.remove();
             
-            actualizarTotalPrecio()
-        }
+        //     actualizarTotalPrecio()
+        // }
 
     }
 }
--- src/vistas/home.js
+++ src/vistas/home.js
@@ -4,17 +4,13 @@ import { tabla } from "../componentes/tabla";
 export const Home =  {
     template:  //html
     `
-    <div class="container mt-3 p-5 border shadow-lg ">
-        <h2 class="text-center">Birras y Tapas</h2>
-        <div id="formPedido"></div>
-        <div id="tablaPedido"></div>
-        <button id="btnEnviar" class="btn btn-primary text-light">Enviar pedido</button>
-    </div>
+    <div id="formPedido" class="container mt-3 p-5 border shadow-lg "></div>
+    <div id="tablaPedidos" class="container mt-5 mb-5 p-5 border shadow-lg"></div>
     `,
     script : ()=>{
         document.querySelector('#formPedido').innerHTML= form.template
         form.script()
-        document.querySelector('#tablaPedido').innerHTML= tabla.template
+        document.querySelector('#tablaPedidos').innerHTML= tabla.template
         tabla.script()
     }
 }
